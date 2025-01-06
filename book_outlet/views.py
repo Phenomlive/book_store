9ba8c63c6ace0475968a33cb2f302183fff4dcc3
Fuad <fuadreddhow@gmail.com>
2025-01-06 19:33:12 +0600

    optimized the performance

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
 
 # Create your views here.
@@ -7,4 +7,19 @@ def index(request):
     books = Book.objects.all()
     return render(request, 'book_outlet/index.html', {
         'books': books
+    })
+
+def book_detail(request, slug):
+    # try:
+    #     book = Book.objects.get(id=id)
+    # except:
+    #     raise Http404
+
+    book = get_object_or_404(Book, slug=slug)
+    
+    return render(request, 'book_outlet/book-detail.html', {
+        'title': book.title,
+        'author': book.author,
+        'rating': book.rating,
+        'is_bestselling': book.is_bestselling
     })
